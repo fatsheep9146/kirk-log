69d945282d2861968bccb9056658ee9d1eb49262
fatsheep9146 <zhaoziqi9146@gmail.com>
2018-06-19 10:29:31 +0800

    Add some neccessary logs into schedule function

--- pkg/schedule.go
+++ pkg/schedule.go
@@ -3,10 +3,20 @@ package logmanager
 import (
 	"github.com/fatsheep9146/kirklog/pkg/agent"
 	"github.com/fatsheep9146/kirklog/pkg/api"
+	log "github.com/sirupsen/logrus"
 )
 
 // Currently we schedule the logSource to the agent with the smallest count of logSources
 func schedule(logsource *api.LogSource, logAgentsMap map[string]*agent.Agent, match map[string]*Match) {
+	logger := log.WithFields(log.Fields{
+		"func": "schedule",
+	})
+	logger.Infof("Start scheduling the logsource %s", logsource.Meta.Name)
+	// logger.Debug("Candidate log agent includes:")
+	// for _, agent := range logAgentsMap {
+	// 	logger.Debugf("  Candidate log agent %s", agent.Name)
+	// }
+
 	counts := make(map[string]int)
 
 	for k, a := range logAgentsMap {
@@ -18,6 +28,9 @@ func schedule(logsource *api.LogSource, logAgentsMap map[string]*agent.Agent, ma
 		}
 		counts[k] = count
 	}
+	// for k, count := range counts {
+	// 	logger.Debugf("The count of agent %s is %d", k, count)
+	// }
 
 	var minCount int
 	var minAgent string = ""
@@ -33,6 +46,8 @@ func schedule(logsource *api.LogSource, logAgentsMap map[string]*agent.Agent, ma
 			}
 		}
 	}
+
+	logger.Infof("Log agent %s is chosen", minAgent)
 
 	match[logsource.Meta.Name].AgentName = minAgent
 }
